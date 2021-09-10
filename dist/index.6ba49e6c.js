b6675039f0ace356e9abe35834cf78d7898b74e8
Abdelazizkb <korbaabdo@gmail.com>
2021-09-10 13:58:08 +0100

    final

--- dist/index.6ba49e6c.js
+++ dist/index.6ba49e6c.js
@@ -455,7 +455,7 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}]},["csun9"], null, "parcelRequire94c2")
-function sendMail(params) {
+function sendMail(e) {
     var tempParams = {
         name: document.getElementById("firstname").value + " " + document.getElementById("lastname").value,
         email: document.getElementById("email").value,
@@ -468,6 +468,19 @@ function sendMail(params) {
     document.getElementById("lastname").value = "";
     document.getElementById("email").value = "";
     document.getElementById("message").value = "";
+    return false;
+}
+window.onscroll = function() {
+    myFunction();
+};
+// Get the header
+var header = document.getElementById("myHeader");
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > 100) header.classList.add("shadow-md");
+    else header.classList.remove("shadow-md");
 }
 
 //# sourceMappingURL=index.6ba49e6c.js.map
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function sendMail(params) {
+function sendMail(e) {
+
+    
     var tempParams = {
         name: document.getElementById("firstname").value + " " + document.getElementById("lastname").value,
         email:document.getElementById("email").value,
@@ -11,5 +13,22 @@ function sendMail(params) {
     document.getElementById("lastname").value = "";
     document.getElementById("email").value = "";
     document.getElementById("message").value = "";
+    return false;
 }
 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > 100 ) {
+    header.classList.add("shadow-md");
+  } else {
+    header.classList.remove("shadow-md");
+  }
+};
